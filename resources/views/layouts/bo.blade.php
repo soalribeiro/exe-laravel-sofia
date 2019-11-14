d4ff9175db488d211a6a42a4fe9017a6916e28db
Sofia Ribeiro <sribeiro@ubiwhere.pt>
2019-11-14 23:43:10 +0000

    final commit

--- resources/views/layouts/bo.blade.php
+++ resources/views/layouts/bo.blade.php
@@ -83,6 +83,10 @@
                             </a>
 
                             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                @if(Auth::user()->role->name == 'admin')
+                                <a class="dropdown-item" href="{{ route('admin') }}">Backoffice</a>
+                                @endif
+                                
                                 <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                     {{ __('Logout') }}
